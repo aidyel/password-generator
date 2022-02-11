7130e5f97d0b5db6c2f46f2f8232b027a29b8fc4
aidyel <aidayeli@icloud.com>
2022-02-11 13:20:09 -0500

    added prompts

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,26 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  console.log("Hey! you clicked the button")
+
+   // Prompt the user for the password criteria
+   var passwordSizeLenght = window.prompt ("Enter size of password")
+   console.log(passwordSizeLenght)
+
+   var IsLowerCase = window.confirm ("Does this password includes lower case?")
+   console.log(IsLowerCase)
+
+    
+ var isUpperCase = window.confirm ("Does this password include upper case?")
+ console.log(isUpperCase)
+
+  
+ var specialCharacters = window.confirm ("Does this password includes special characters?")
+ console.log(specialCharacters)
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
